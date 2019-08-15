cd3ad576a91f6fef7926c1cb678a9f3f5bf6ee8e
Kálmán Bianka <kalman.bianka97@gmail.com>
2019-08-15 18:37:49 +0200

    added + and - buttons to inc or decr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  counter: number = 0;
+
+
+onIncrementCounter(){
+  if(this.counter !== 10){
+  this.counter ++;
+  }
+}
+
+onDecrementCounter(){
+  if(this.counter !== 0){
+  this.counter--;
+  }
+}
 }
